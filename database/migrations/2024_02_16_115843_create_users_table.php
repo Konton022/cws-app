7bb3a3c684333c60fd73f9cd0459506b41933b55
Konton022 <42418105+Konton022@users.noreply.github.com>
2024-02-19 09:45:57 +0500

    migrations

--- database/migrations/2024_02_16_115843_create_users_table.php
+++ database/migrations/2024_02_16_115843_create_users_table.php
@@ -13,12 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('birthday')->nullable();
+            $table->boolean('status')->default(true);
+            $table->timestamps();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
         });
     }
 
